1d73414dc3ed4ec11983dfabf07d9dfa8a9d2c8c
Ana Lima <ayf.lima@gmail.com>
2022-10-01 20:26:33 -0400

    Customize application

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,66 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { NavLink } from "react-router-dom";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          HackMIT<br></br>
+          Immersive NeRF
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <nav
+      className="navbar is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <a
+            role="button"
+            className={`navbar-burger burger ${isOpen && "is-active"}`}
+            aria-label="menu"
+            aria-expanded="false"
+            onClick={() => setOpen(!isOpen)}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div className={`navbar-menu ${isOpen && "is-active"}`}>
+          <div className="navbar-start">
+
+            <NavLink
+              className="navbar-item"
+              // activeClassName="is-active"
+              to="/about"
+            >
+              About
+            </NavLink>
+
+          </div>
+
+          <div className="navbar-end">
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-white">Log in</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    
     </div>
+
   );
 }
 
---
+++ src/pages/Upload.tsx
@@ -0,0 +1,7 @@
+export const Update = () => {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
