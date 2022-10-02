84da74980dc2705201880bc0545237e1523e6954
Ana Lima <ayf.lima@gmail.com>
2022-10-02 03:47:16 -0400

    Add a frontend prototype

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import logo from './logo.svg';
+import logo from './assets/hackmit_logo.png';
+import VideoInput from "./components/VideoInput";
 import './App.css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+
 
 function App() {
+  const handlePageChange = () => {
+    window.location.href="https://github.com/anayflima/immersive-NeRF-api"
+  }
   return (
     <div className="App">
+      <img className="project-logo" src={logo} alt="Logo" />
       <header className="App-header">
         <p>
-          HackMIT
+          Immersive NeRF
         </p>
       </header>
+      <main className="App-main">
+        Building your own metaverse. Some description of the project
+        <VideoInput width={400} height={300} />
+      </main> 
+      <footer className="App-footer">
+        <div class="github" >
+        <span >
+          <GitHubIcon onClick={handlePageChange}></GitHubIcon> &nbsp; &nbsp;
+        </span>
+        <span class="developers" >
+        Project developed during HackMIT 2022 by 
+        Ana Yoon - Ashley Neall - Joel Julin - John Lee
+        </span>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/components/VideoInput.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./styles.css";
+import IconButton from '@mui/material/IconButton';
+import UploadIcon from '@mui/icons-material/Upload';
+
+export default function VideoInput(props) {
+  const { width, height } = props;
+
+  const inputRef = React.useRef();
+
+  const [source, setSource] = React.useState();
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    setSource(url);
+  };
+
+  const handleChoose = (event) => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className="VideoInput">
+      <input
+        ref={inputRef}
+        className="VideoInput_input"
+        type="file"
+        onChange={handleFileChange}
+        accept=".mov,.mp4"
+      />
+      <p>
+      {!source &&
+        <IconButton onClick={handleChoose}>
+          Upload a video
+        <UploadIcon />
+        </IconButton>}
+      </p>
+      {source && (
+        <video
+          className="VideoInput_video"
+          width="100%"
+          height={height}
+          controls
+          src={source}
+        />
+      )}
+      <div className="VideoInput_footer">{source}</div>
+    </div>
+  );
+}
